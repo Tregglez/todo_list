078752abfbdd573da150ff5449bfcb1139ec5669
benjamin treglown <treggy89@gmail.com>
2022-12-22 14:19:52 +1100

    rails new with db

---
+++ db/schema.rb
@@ -0,0 +1,10 @@
+ActiveRecord::Schema[7.0].define(version: 2022_12_22_031417) do
+  create_table "todos", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.boolean "completed"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
