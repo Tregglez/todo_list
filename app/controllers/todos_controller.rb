482dca38c3c41909a37553848fcc43187374ee61
benjamin treglown <treggy89@gmail.com>
2022-12-29 00:43:27 +1100

    Create new function working, styling for home page finished

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,40 @@
+class TodosController < ApplicationController
+  before_action :set_todo, only: %i[edit update destroy]
+
+  def index
+    @todos = Todo.all
+  end
+
+  def new
+    @todo = Todo.new
+  end
+
+  def create
+    @todo = Todo.new(todo_params)
+    if @todo.save
+      redirect_to todos_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @todo.update(todo_params)
+    redirect_to todos_path
+  end
+
+  def destroy
+    @todo.destroy
+    redirect_to todos_path, status: :see_other
+  end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:title, :description)
+  end
+
+  def set_todo
+    @todo = Todo.find(params[:id])
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -6,5 +6,4 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_22_031417) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 end
